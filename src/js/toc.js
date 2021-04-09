72e4a0643486d7da35b438d6165420e5b38d42c4
Alex <alex.zzh.s7@gmail.com>
2021-04-09 10:21:11 +0000

    Minor fixes

--- src/js/toc.js
+++ src/js/toc.js
@@ -3,14 +3,17 @@ let current = null
 
 const callback = e => {
   e.forEach(i => {
-    if (i.isIntersecting === true) {
-      if (current) current.classList.remove('toc-active')
+    if (i.isIntersecting === true && i.boundingClientRect.top >= 10) {
+      Array.from(document.querySelectorAll('.toc a')).forEach(i => i.classList.remove('toc-active'))
       current = document.querySelector(`.toc a[href="#${i.target.id}"]`)
       current.classList.add('toc-active')
-    } else if (i.boundingClientRect.top >= 10 && current) {
+    } else if (i.boundingClientRect.top >= 10 && current && i.target.id === current.getAttribute('href').replace(/^#/, '')) {
       current.classList.remove('toc-active')
-      const previous = current.parentElement.previousElementSibling
-      if (previous) previous.querySelector('a').classList.add('toc-active')
+      const previous = current.parentElement.previousElementSibling && current.parentElement.previousElementSibling.querySelector('a')
+      if (previous) {
+        current = previous
+        previous.querySelector('a').classList.add('toc-active')
+      }
     }
   })
 }
