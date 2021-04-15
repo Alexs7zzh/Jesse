d98d035a64db534037d8a45a99b075827cc9ea94
Alex <alex.zzh.s7@gmail.com>
2021-04-15 05:34:40 +0000

    Inline JS

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -1,5 +1,4 @@
 module.exports = config => {
   config.addPlugin(require('./plugins/sass'))
-  config.addPlugin(require('./plugins/rollup'))
   config.addPlugin(require('@11ty/eleventy-plugin-rss'))
 }
