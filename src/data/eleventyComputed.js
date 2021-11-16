8f079f94d31a14ba0b3f204494758f2f432e9b0f
Alex Zhou <alex.zzh.s7@gmail.com>
2021-11-16 20:01:15 +0900

    Fix permalink

--- src/data/eleventyComputed.js
+++ src/data/eleventyComputed.js
@@ -3,5 +3,9 @@ module.exports = {
     if (data.tags && data.tags[0] && data.tags[0].length !== 0)
       return data.tags[0].split(' ')
     return []
+  },
+  permalink: data => {
+    if (!data.permalink) return `/${data.year}/${data.page.fileSlug}/`
+    return data.permalink
   }
 }
